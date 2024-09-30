30ca81bb0bebf5e39134891a2ac796f170c4a43c
kiennd3105 <kiennd3105@gmail.com>
2024-09-30 19:47:59 +0700

    Delete DATN1_1/DATN1_1 directory

--- DATN1_1/DATN1_1/src/main/java/com/example/demo/repository/HinhThucHoaDonRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.HinhThucHoaDon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HinhThucHoaDonRepo extends JpaRepository<HinhThucHoaDon,String> {
-}
--- DATN1_1/DATN1_1/src/main/java/com/example/demo/repository/NhanVienRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NhanVienRepo extends JpaRepository<NhanVien,String> {
-}
