416eaebbaf116b5de86558870cf2512b8981b8b7
kiennd3105 <kiennd3105@gmail.com>
2024-10-25 23:08:04 +0700

    Delete DATN1_1 directory

--- DATN1_1/src/main/java/com/example/demo/repository/KhachHangRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.KhachHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KhachHangRepo extends JpaRepository<KhachHang,String> {
-}
